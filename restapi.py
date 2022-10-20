1a488a741f28c3333cc4e13cba941c351d4a87ac
Zülfikar Babacan Elmalı <99174001+Skadoosh10@users.noreply.github.com>
2022-10-20 21:48:22 +0300

    Update restapi.py

--- restapi.py
+++ restapi.py
@@ -1,7 +1,5 @@
 from urllib import request
 import requests
-#response = requests.get("https://api.thecatapi.com/v1/images/search")
-#print(response.json())
 
 url = "https://api.thecatapi.com/v1/images/search"
 resp=requests.get(url=url)
